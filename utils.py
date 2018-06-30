794e63d4d6a316c3b0d7a3f57b931da73f24d3c2
Maninder Singh <maniraniyal@gmail.com>
2018-06-30 11:18:33 -0400

    add save_torch_weights_from_model, update requirements, add writing_preemp_tolerant_code.txt

--- utils.py
+++ utils.py
@@ -8,11 +8,31 @@ import cv2
 import numpy as np
 from PIL import Image
 from shutil import copyfile
+import random
+import torch
+import shutil
 
-"""
-convert_grayImages_to_RGB assumes that directory has only grayscale or RGB images, not RGBA.
-"""
-def convert_grayImages_to_RGB(directories_list, out_dir_suffix="_RGB", image_reg="/*.jpg"):
+
+def save_torch_weights_from_model(torch_model, weight_filename):
+    if os.path.exists(weight_filename):
+        for i in range(10):
+            tmp_file = weight_filename + '_' + str(random.randint(1, 1000))
+            if not os.path.exists(tmp_file):
+                break
+        else:
+            raise Exception("Could not generate unique random file")
+        torch.save(torch_model.state_dict(), tmp_file)
+        # os.remove(weight_file)
+        # os.rename(tmp_file, weight_file)
+        shutil.move(tmp_file, weight_filename)
+    else:
+        torch.save(torch_model.state_dict(), weight_filename)
+
+
+def convert_gray_images_to_rgb(directories_list, out_dir_suffix="_RGB", image_reg="/*.jpg"):
+    """
+    convert_grayImages_to_RGB assumes that directory has only grayscale or RGB images, not RGBA.
+    """
     for d in directories_list:
         os.mkdir(d+out_dir_suffix)
         for filename in glob.glob(d+image_reg):
@@ -25,10 +45,10 @@ def convert_grayImages_to_RGB(directories_list, out_dir_suffix="_RGB", image_reg
                     copyfile(filename, d+out_dir_suffix+filename.split("/")[-1])
                                
 
-"""
-convert_RGB_to_grayImages assumes that directory has only grayscale or RGB images, not RGBA.
-"""
-def convert_RGB_to_grayImages(directories_list, out_dir_suffix="_GRAY", image_reg="/*.jpg"):
+def convert_rgb_to_gray_images(directories_list, out_dir_suffix="_GRAY", image_reg="/*.jpg"):
+    """
+    convert_RGB_to_grayImages assumes that directory has only grayscale or RGB images, not RGBA.
+    """
     for d in directories_list:
         os.mkdir(d+out_dir_suffix)
         for filename in glob.glob(d+image_reg):
@@ -40,35 +60,39 @@ def convert_RGB_to_grayImages(directories_list, out_dir_suffix="_GRAY", image_re
                 else:
                     copyfile(filename, d+out_dir_suffix+filename.split("/")[-1])
                     
-"""
-saves the numpy array to the disk.
-"""
+
 def save_array(rootdir, array):
+    """
+    saves the numpy array to the disk.
+    """
     c=bcolz.carray(array, rootdir=rootdir, mode='w')
     c.flush()
 
-"""
-Loads numpy array from disk
-"""
+
 def load_array(rootdir):
+    """
+    Loads numpy array from disk
+    """
     return bcolz.open(rootdir)[:]
 
-"""
-plotImageFromFiles plot images in row and column, given filename with abs/relaive path
-"""
-def plotImageFromFiles(filenames,col=3,DPI=200):
+
+def plot_image_from_files(filenames, col=3, DPI=200):
+    """
+    plotImageFromFiles plot images in row and column, given filename with abs/relaive path
+    """
     total_images = len(filenames)
     fig = plt.figure(dpi=DPI)
     for i,f in enumerate(filenames):
         plt.subplot(int(total_images/col)+1,col,i+1)
         plt.imshow(np.asarray(Image.open(filenames[i])))
 
-"""
-plotImageFromListOfArrays plot images in row and column, given numpyarray(3,x,x) as list.
-"""
-def plotImageFromListOfArrays(arraylist,col=3,DPI=200):
+
+def plot_image_from_list_of_arrays(arraylist, col=3, DPI=200):
+    """
+    plotImageFromListOfArrays plot images in row and column, given numpyarray(3,x,x) as list.
+    """
     total_images = len(arraylist)
     fig = plt.figure(dpi=DPI)
-    for i,arr in enumerate(arraylist):
+    for i, arr in enumerate(arraylist):
         plt.subplot(int(total_images/col)+1,col,i+1)
         plt.imshow(arr)
