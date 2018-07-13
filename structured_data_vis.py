792d8d31fc9f325defa70bd8f4213b9f9fb5a565
maniraniyal <maniraniyal@gmail.com>
2018-07-13 12:30:56 -0400

    Update structured_data_vis.py

--- structured_data_vis.py
+++ structured_data_vis.py
@@ -7,3 +7,10 @@ def find_col_distinct_values(df, include=None, exclude=['O']):
                               'UQV': list(set(df[col])),
                               }
     return dict_frequency
+
+def find_all_zero_col(df, include=None, exclude=['O']):
+    all_zero_col = []
+    for col in df.select_dtypes(include=include, exclude=exclude):
+        if not len(set(df[df[col] != 0][col])):
+            all_zero_col.append(col)
+    return all_zero_col
